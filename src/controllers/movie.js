542ee54c79bb9ed3e829543e7234755c298d7cb6
bravint <bravint@gmail.com>
2022-01-31 12:47:32 +0000

    requirement 2: part complete

--- src/controllers/movie.js
+++ src/controllers/movie.js
@@ -1,10 +1,44 @@
-const { Prisma, PrismaClient } = require('@prisma/client')
+const { Prisma, PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-const getAllMovies =  async (req, res) => {
-    const movies = await prisma.movie.findMany()
-    return res.json(movies)
-  }
+const getAllMovies = async (req, res) => {
+    const response = await prisma.movie.findMany();
+    return res.json(response);
+};
 
-module.exports = {getAllMovies}
+const createMovie = async (req, res) => {
+    const { title, runtimeMins, screenings } = req.body;
+    let response;
+    if (screenings === undefined) {
+        response = await createMovieWithoutScreening(title, runtimeMins);
+    } else {
+        response = await createMovieWithScreening(
+            title,
+            runtimeMins,
+            screenings
+        );
+    }
+    return res.json(response);
+};
+
+const createMovieWithoutScreening = async (title, runtimeMins) => {
+    return (response = await prisma.movie.create({
+        data: {
+            title,
+            runtimeMins,
+        },
+    }));
+};
+
+const createMovieWithScreening = async (title, runtimeMins, screenings) => {
+    return (response = await prisma.movie.create({
+        data: {
+            title,
+            runtimeMins,
+            screenings,
+        },
+    }));
+};
+
+module.exports = { getAllMovies, createMovie };
