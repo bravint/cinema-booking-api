b3e57cdf0c7453ac595552204a08cc37e8d0a18e
bravint <bravint@gmail.com>
2022-02-09 15:02:20 +0000

    requirement 1 completed

--- src/controllers/movie.js
+++ src/controllers/movie.js
@@ -9,21 +9,25 @@ const getAllMovies = async (req, res) => {
         response = await getAll();
     }
 
-    return res.json(response);
+    if (response.length === 0) {
+        return res.status(200).json(`No movies found`);
+    } else {
+        return res.json(response);
+    }
 };
 
 const filterByRuntime = async (query) => {
-    let minValue;
-    let maxValue;
+    let gt;
+    let lt;
 
-    query.gt === undefined ? (minValue = 1) : (minValue = parseInt(query.gt, 10));
-    query.lt === undefined ? (maxValue = 100000) : (maxValue = parseInt(query.lt, 10));
+    (!query.gt) ? (minValue = 1) : (gt = parseInt(query.gt, 10));
+    (!query.lt) ? (maxValue = 1000) : (lt = parseInt(query.lt, 10));
 
     return await prisma.movie.findMany({
         where: {
             runtimeMins: {
-                gt: minValue,
-                lt: maxValue,
+                gt,
+                lt,
             },
         },
         include: { screenings: true },
@@ -47,10 +51,10 @@ const getMovie = async (req, res) => {
         response = await findMovieById(parseInt(movie, 10));
     }
 
-    if (response.length === 0) {
+    if (!response.length) {
         return res.status(404).send(`No match for ${movie} found`);
     } else {
-        return res.json(response)
+        return res.json(response);
     }
 };
 
@@ -58,7 +62,7 @@ const createMovie = async (req, res) => {
     const { title, runtimeMins, screenings } = req.body;
 
     const checkForExistingMovie = await findMovieByTitle(title);
-    if (checkForExistingMovie.length > 0) return res.status(400).send(`${title} already exists in database`);
+    if (checkForExistingMovie.length > 0) return res.status(400).json(`${title} already exists in database`);
 
     let response;
 
@@ -114,72 +118,86 @@ const findMovieById = async (id) => {
     });
 };
 
+
 const updateMovie = async (req, res) => {
     const id = idToInteger(req.params);
-    const { title, runtimeMins } = req.body;
+    let { title, runtimeMins } = req.body;
 
-    const updateToScreenings = req.body.screenings;
+    let updateToScreenings = req.body.screenings;
     let screenings = [];
 
     for (let i = 0; i < updateToScreenings.length; i++) {
         if (updateToScreenings.id) {
-            const updatedScreening = await updateScreenings(updateToScreenings[i]);
+            const updatedScreening = await updateScreenings(updateToScreenings[i], res);
             screenings.push(updatedScreening);
-        } else {
-            const updatedScreening = await createScreenings(updateToScreenings[i],id);
-            screenings.push(updatedScreening);
-        }
+            continue
+        } 
+        const updatedScreening = await createScreenings(updateToScreenings[i], id);
+        screenings.push(updatedScreening);
     }
 
     const response = await updatedMovie(title, runtimeMins, id);
 
-    res.json({ ...response, screenings });
+    if (response === `error`) return res.status(404).json(`No movie at id ${id}`);
+ 
+    return res.json({ ...response, screenings });
+    
 };
 
 const updatedMovie = async (title, runtimeMins, id) => {
-    return await prisma.movie.update({
-        where: {
-            id,
-        },
-        data: {
-            title,
-            runtimeMins,
-        },
-    });
+    try {
+        return await prisma.movie.update({
+            where: {
+                id,
+            },
+            data: {
+                title,
+                runtimeMins,
+            },
+        });
+    } catch {
+        return `error`;
+    }
 };
 
 const updateScreenings = async (screening) => {
     const { startsAt, screenId, id } = screening;
-
-    return await prisma.screening.update({
-        data: {
-            startsAt,
-            screenId,
-        },
-        where: {
-            id,
-        },
-    });
+    try {
+        return await prisma.screening.update({
+            data: {
+                startsAt,
+                screenId,
+            },
+            where: {
+                id,
+            },
+        });
+    } catch {
+        return `error`;
+    }
 };
 
 const createScreenings = async (screening, id) => {
     const { startsAt, screenId } = screening;
-
-    return await prisma.screening.create({
-        data: {
-            startsAt,
-            screen: {
-                connect: {
-                    id: screenId,
+    try {
+        return await prisma.screening.create({
+            data: {
+                startsAt,
+                screen: {
+                    connect: {
+                        id: screenId,
+                    },
+                },
+                movie: {
+                    connect: {
+                        id,
+                    },
                 },
             },
-            movie: {
-                connect: {
-                    id,
-                },
-            },
-        },
-    });
+        });
+    } catch {
+        return `error`;
+    }
 };
 
 const createScreen = async (req, res) => {
